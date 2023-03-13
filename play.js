c8d02e8ae11854122d8f0a3bf46394e86d0b941d
sanket <sanket.panda@viacom18.com>
2023-03-13 22:52:07 +0530

    Object Properties and Methods

--- play.js
+++ play.js
@@ -1,30 +1,11 @@
-const name = "Max";
-let age = 29;
-const hasHobbies = true;
-
-age = 30;
-
-const summerizeUser = (userName, userAge, userHasHobbies) => {
-    return (
-      "Name is " +
-      userName +
-      ", age is " +
-      userAge +
-      " and user has hobbies: " +
-      userHasHobbies
-    );
+const person = {
+  name: 'Max',
+  age: 29,
+  greet() {
+    console.log('Hi, I am ' + this.name);
+  }
 }
 
-const add = (a, b) => a+b;
-
-const addOne = a => a + 1;
-
-const addRandom = () => 1 + 2;
-
-console.log(add(1,2));
-console.log(addOne(1));
-console.log(addRandom());
-
-
-console.log(summerizeUser(name, age, hasHobbies));
+person.greet();
+// console.log(person);
 
