1819a62fc945414c79ac9949f9ea83e45c8f8da6
sanket <sanket.panda@viacom18.com>
2023-03-13 22:55:48 +0530

    Arrays and Array Methods

--- play.js
+++ play.js
@@ -6,6 +6,13 @@ const person = {
   }
 }
 
-person.greet();
-// console.log(person);
+const hobbies = ['sports', 'cooking'];
+
+// for(let hobby of hobbies){
+//   console.log(hobby);
+// }
+
+console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
+console.log(hobbies);
+
 
