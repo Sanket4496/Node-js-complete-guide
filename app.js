ef5f5ed8ab44363de01884489587750cb90d814a
sanket <sanket.panda@viacom18.com>
2023-03-20 13:39:34 +0530

    using helper function for Navigation

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const rootDir = require('./util/path');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
@@ -13,7 +14,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', 'page-not-found.html'));
+    res.status(404).sendFile(path.join(rootDir, 'views', 'page-not-found.html'));
 });
 
 app.listen(3000);
