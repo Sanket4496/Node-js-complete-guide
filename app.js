2964523493fc4e0bff484345f9e4bdda4d27443b
sanket <sanket.panda@viacom18.com>
2023-03-20 11:53:24 +0530

    How Middleware Works and Express.js: behind scenes

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
@@ -11,9 +9,8 @@ app.use((req, res, next) => {
 
 app.use((req, res, next)=>{
     console.log('In the another middleware');
+    res.send('<h1>Hello from Express</h1>');
     //...
-})
+});
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
