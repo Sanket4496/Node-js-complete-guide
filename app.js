62af8466b8c3228671579189d131d473450803b9
sanket <sanket.panda@viacom18.com>
2023-03-20 11:45:19 +0530

    Adding Middleware

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log('In the middleware');
+    next(); // Allows the request to continue to the next middleware
+});
+
+app.use((req, res, next)=>{
+    console.log('In the another middleware');
+    //...
+})
+
 const server = http.createServer(app);
 
 server.listen(3000);
