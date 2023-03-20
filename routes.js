934d4cb2870bf4f0d1698c7588acb69550cca75e
sanket <sanket.panda@viacom18.com>
2023-03-20 11:20:53 +0530

    till section 4 node js complete course

---
+++ routes.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+
+const requestHandler = (req, res) => {
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>My First Page</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
+        res.write('</html>')
+        return res.end();
+    }
+    if (url === '/message' && method === 'POST'){
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[0];
+            fs.writeFile('message.txt', message, (err) => {
+                res.writeHead(302, {'Location': '/'});
+                return res.end();
+            });
+        });
+    }
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hell from my Node.js Server!</h1></body>')
+    res.write('</html>')
+    res.end();
+};
+
+// module.exports = requestHandler;
+
+// module.exports = {
+//     handler : requestHandler,
+//     someText: 'Some hard coded text'
+// }
+
+module.exports = requestHandler
