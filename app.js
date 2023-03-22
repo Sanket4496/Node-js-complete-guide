b6411e81b3da941cbdb15852750c2a3a3e9ceeea
sanket <sanket.panda@viacom18.com>
2023-03-22 22:31:26 +0530

    Adding the HTML and CSS Files for home page and Add Product Page

--- app.js
+++ app.js
@@ -4,14 +4,19 @@ const path = require('path');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+//Setting pug as view engine
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const rootDir = require('./util/path');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
