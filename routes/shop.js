1f4b7f1df9c5998d1f80c5149adef43ca22b5051
sanket <sanket.panda@viacom18.com>
2023-05-10 13:40:37 +0530

    Adding Controllers

--- routes/shop.js
+++ routes/shop.js
@@ -1,21 +1,10 @@
 const express = require("express");
 const path = require("path");
 
-const rootDir = require("../util/path");
-const adminData = require("./admin");
+const productController = require('../controllers/products')
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-    const products = adminData.products;
-    res.render("shop", {
-      prods: products,
-      docTitle: "shop",
-      path: "/",
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true
-    });
-});
+router.get("/", productController.getProducts);
 
 module.exports = router;
